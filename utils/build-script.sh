37e2b7b807b9245f4c2654fd445081855ae5ab04
R Steiner <cranndarach@users.noreply.github.com>
2016-12-11 12:28:12 -0500

    Change 'mkdir build' to 'mkdir bin'.

--- utils/build-script.sh
+++ utils/build-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "#!/usr/bin/env node" > bin/safebot || (mkdir build && echo "#!/usr/bin/env node" > bin/safebot)
+echo "#!/usr/bin/env node" > bin/safebot || (mkdir bin && echo "#!/usr/bin/env node" > bin/safebot)
 echo >> bin/safebot
 cat ./main.js >> bin/safebot
